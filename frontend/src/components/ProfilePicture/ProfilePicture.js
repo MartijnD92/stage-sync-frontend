1cfc3495a1b009925ac4587bc6a202a58e828578
Martijn Doensen <martijn.doensen@live.nl>
2021-05-29 13:29:31 +0200

    Remove all files but README from main branch

--- frontend/src/components/ProfilePicture/ProfilePicture.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import DefaultProfile from 'assets/default-profile.jpg';
-import './css/ProfilePicture.scss';
-
-export default function ProfilePicture({ defaultPicture }) {
-	return (
-		<>{defaultPicture && <img src={DefaultProfile} alt="default profile" />}</>
-	);
-}
