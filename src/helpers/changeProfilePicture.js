585859a9e2ace60a9cfe3f26d3332b247c33bcd4
Martijn Doensen <martijn.doensen@live.nl>
2021-06-29 20:14:49 +0200

    Fix profile picture getter

--- src/helpers/changeProfilePicture.js
+++ src/helpers/changeProfilePicture.js
@@ -1,21 +1,24 @@
-
 import axios from 'axios';
 
-export default async function changeProfilePicture(picture, user, toggleUpdate) {
-    
-    const formData = new FormData();
-    formData.append('file', picture);
+export default async function changeProfilePicture(
+	picture,
+	user,
+	toggleUpdate
+) {
+	const formData = new FormData();
+	formData.append('file', picture);
 
-    try {
-        await axios.put(
-            `http://localhost:8080/api/users/user/profilepicture`, formData,
-            {
-                headers: {
-                    Authorization: `Bearer ${user.token}`,
-                },
-            }
-        );
-    } catch(e) {
-        console.error(e)
-    }
-};
+	try {
+		await axios.post(
+			`http://localhost:8080/api/users/user/profilepicture`,
+			formData,
+			{
+				headers: {
+					Authorization: `Bearer ${user.token}`,
+				},
+			}
+		);
+	} catch (e) {
+		console.error(e);
+	}
+}
